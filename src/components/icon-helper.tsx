d36fddc2b22d8870758191437189595c84bd6c20
ParthKal1712 <erp@pragatilogistics.com>
2024-05-28 00:14:00 -0600

    Misc

--- src/components/icon-helper.tsx
+++ src/components/icon-helper.tsx
@@ -4,16 +4,16 @@ import * as LucideIcons from "lucide-react";
 // Define a type for the LucideIcons object
 type LucideIconComponent = keyof typeof LucideIcons;
 
-const Icon: React.FC<{ name: LucideIconComponent; className?: string }> = ({
-  name,
-  className,
-  ...props
-}) => {
+const Icon: React.FC<{
+  name: LucideIconComponent;
+  size?: number;
+  className?: string;
+}> = ({ name, size, className, ...props }) => {
   // Check if the icon exists in LucideIcons
   if (LucideIcons[name]) {
     // Dynamically render the Lucide icon component
     const IconComponent = LucideIcons[name] as React.ComponentType<any>;
-    return <IconComponent className={className} {...props} />;
+    return <IconComponent className={className} size={size} {...props} />;
   } else {
     // Return null or handle the case where the icon doesn't exist
     console.warn(`Icon '${name}' does not exist.`);
--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -35,6 +35,7 @@ import {
 } from "@/components/ui/dialog";
 import { Label } from "./ui/label";
 import MyProfileDialog from "./my-profile-dialog";
+import Icon from "./icon-helper";
 
 function TopBar() {
   // Load the theme, router and session
@@ -74,7 +75,8 @@ function TopBar() {
         </SheetContent>
       </Sheet>
       <div className="w-full flex-1">
-        <form>
+        {/* <Icon name="LoaderCircle" size={30} className="animate-spin" /> */}
+        {/* <form>
           <div className="relative">
             <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
             <Input
@@ -83,7 +85,7 @@ function TopBar() {
               className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
             />
           </div>
-        </form>
+        </form> */}
       </div>
       <Dialog>
         <DropdownMenu>
